ec19a99857585624773238d33b3ba30b07d92278
favianhanasta <favianhanastap@gmail.com>
2022-04-14 16:40:29 +0700

    PHAR-15-favian : API untuk proses transaksi custom order

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -11,7 +11,7 @@ module.exports={
                     let {iduser,invoice} = JSON.parse(req.body.data);
                     let {filename} = req.files[0];
                     console.log('filename',filename)
-                    await dbQuery(`insert into orderbyresep values (null,${iduser},'/images/${filename}','${invoice}',3);`);
+                    await dbQuery(`insert into orderbyresep values (null,${iduser},'/images/${filename}','${invoice}',8);`);
                     res.status(200).send({
                         success : true,
                         message : "upload success",
@@ -112,6 +112,86 @@ module.exports={
                 success : false,
                 message: 'admin action failed',
                 error
+            })
+        }
+    },
+    getOrderbyresep : async (req,res)=>{
+        try{
+            let {idrole,iduser} = req.dataUser;
+            let {idorder} = req.query;
+            let getOrderbyresep = await dbQuery(`SELECT o.*, s.status,u.* from orderbyresep o join status s on o.idstatus=s.idstatus join user u on o.iduser=u.iduser ${idrole==2? `where iduser=${iduser} and o.idstatus=8` : `${idorder?`where idorder=${idorder}`:''}`};`);
+            res.status(200).send({
+                message : 'Get order by resep sukses',
+                success : true,
+                dataGetOrder : getOrderbyresep
+            })
+        }
+        catch (error){
+            console.log('error get order by resep', error);
+            res.status(500).send({
+                message : 'order by resep error',
+                success : false,
+                error
+            })
+        }
+    },
+    addToCartResep : async (req,res)=>{
+        try {
+            let {idorder,iduser,idproduct,qty,idsatuan} = req.body;
+            await dbQuery(`insert into cartresep values (null,${idorder},${iduser},${idproduct},${qty},${idsatuan});`);
+            res.status(200).send({
+                message : 'add to cartresep berhasil',
+                success : true,
+                error :""
+            })
+        } catch (error) {
+            console.log('error add cart resep',error);
+            res.status(500).send({
+                message : 'add to cart resep error',
+                success : true,
+                error
+            })
+        }
+    },
+    getCartResep : async (req,res)=>{
+        try {
+            let getCart = await dbQuery(`select c.*,p.*,i.url,s.satuan from cartresep c join product p on c.idproduct=p.idproduct join imageproduct i on c.idproduct = i.idproduct join satuan s on c.idsatuan = s.idsatuan where c.idorder = ${req.query.idorder};`);
+            let getStock = await dbQuery(`SELECT s.*, c.idproduct FROM stock s JOIN cart c ON s.idproduct=c.idproduct;`)
+            getCart.forEach(async (value, index) => {
+                value.stock = [];
+                getStock.forEach((val, idx) => {
+                    if (value.idcart == val.idcart) {
+                        value.stock.push(val)
+                    }
+                })
+            })
+            res.status(200).send({
+                message : 'get cart resep success',
+                success : true,
+                error : '',
+                dataCartResep : getCart
+            })
+        } catch (error) {
+            console.log('getcart resep error', error);
+            res.status(500).send({
+                message : 'get cart resep error',
+                success : failed,
+                error
+            })
+        }
+    },
+    deleteCartResep: async (req,res)=>{
+        try {
+            await dbQuery(`delete from cartresep where idcartresep = ${req.params.id} `);
+            res.status(200).send({
+                message:'delete cart resep sukses',
+                success : true
+            })           
+        } catch (error) {
+            console.log('error delete cart',error)
+            res.status(500).send({
+                message:'error delete cart',
+                success : false
             })
         }
     }
