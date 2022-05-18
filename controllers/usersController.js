246aa8e8d8cf650c693a0f8e6bdaa0ec0df7c282
reyhanbalthazar <93903964+reyhanbalthazar@users.noreply.github.com>
2022-05-18 18:45:48 +0700

    PHAR-17-Reyhan : fix verification

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -140,12 +140,12 @@ module.exports = {
                 await dbQuery(`UPDATE user SET idstatus=2 WHERE iduser=${db.escape(req.dataUser.iduser)};`);
                 let login = await dbQuery(`SELECT * FROM user WHERE iduser=${db.escape(req.dataUser.iduser)};`);
                 if (login.length > 0) {
-                    let { iduser, username, email, password, role, status } = login[0];
-                    let token = createToken({ iduser, username, email, role, status });
+                    let { iduser, username, email, password, idrole, status } = login[0];
+                    let token = createToken({ iduser, username, email, idrole, status });
                     res.status(200).send({
                         success: true,
                         message: "Login Success ✅",
-                        dataVerify: { username, email, role, status, token },
+                        dataVerify: { username, email, idrole, status, token },
                         error: ""
                     })
                 }
